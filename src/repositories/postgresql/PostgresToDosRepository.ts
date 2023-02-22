a6868e630b73e8225a9c352a7ec9f4a419f468a1
Alex Galhardo <alex.vieira@pravaler.com.br>
2023-02-22 16:01:46 -0300

    fix: names

--- src/repositories/postgresql/PostgresToDosRepository.ts
+++ src/repositories/postgresql/PostgresToDosRepository.ts
@@ -6,139 +6,139 @@ import ToDoEntity from "../../entities/ToDoEntity";
 import { IToDosRepository, ToDoRepositoryResponse } from "../../ports/IToDosRepository";
 
 export default class PostgresToDosRepository implements IToDosRepository {
-    private getToDoEntityFromPrismaToDo(queryResponse: ToDo) {
-        return new ToDoEntity(
-            queryResponse.id,
-            queryResponse.user_id,
-            queryResponse.title,
-            queryResponse.description,
-            queryResponse.done,
-            queryResponse.created_at,
-            queryResponse.updated_at,
-        );
-    }
+	private getToDoEntityFromPrismaToDo (queryResponse: ToDo): ToDoEntity {
+		return new ToDoEntity(
+			queryResponse.id,
+			queryResponse.user_id,
+			queryResponse.title,
+			queryResponse.description,
+			queryResponse.done,
+			queryResponse.created_at,
+			queryResponse.updated_at,
+		);
+	}
 
-    private getToDoSEntitiesFromPrismaToDos(queryResponse: ToDo[]) {
-        const toDoesEntities: ToDoEntity[] = [];
+	private getToDoSEntitiesFromPrismaToDos (queryResponse: ToDo[]): ToDoEntity[] {
+		const toDosEntities: ToDoEntity[] = [];
 
-        queryResponse.forEach((todo: ToDo) => {
-            toDoesEntities.push(
-                new ToDoEntity(
-                    todo.id,
-                    todo.user_id,
-                    todo.title,
-                    todo.description,
-                    todo.done,
-                    todo.created_at,
-                    todo.updated_at,
-                ),
-            );
-        });
+		queryResponse.forEach((todo: ToDo) => {
+			toDosEntities.push(
+				new ToDoEntity(
+					todo.id,
+					todo.user_id,
+					todo.title,
+					todo.description,
+					todo.done,
+					todo.created_at,
+					todo.updated_at,
+				),
+			);
+		});
 
-        return toDoesEntities;
-    }
+		return toDosEntities;
+	}
 
-    async getAllByUserId(userId: string): Promise<ToDoRepositoryResponse> {
-        try {
-            const allToDosFound = await prisma.toDo.findMany({
-                where: {
-                    user_id: userId,
-                },
-            });
+	async getAllByUserId (userId: string): Promise<ToDoRepositoryResponse> {
+		try {
+			const allToDosFound = await prisma.toDo.findMany({
+				where: {
+					user_id: userId,
+				},
+			});
 
-            if (allToDosFound) {
-                return {
-                    success: true,
-                    toDosEntities: this.getToDoSEntitiesFromPrismaToDos(allToDosFound),
-                };
-            }
-        } catch (error) {
-            return {
-                success: false,
-                error,
-            };
-        }
-    }
+			if (allToDosFound) {
+				return {
+					success: true,
+					toDosEntities: this.getToDoSEntitiesFromPrismaToDos(allToDosFound),
+				};
+			}
+		} catch (error) {
+			return {
+				success: false,
+				error,
+			};
+		}
+	}
 
-    async getById(toDoId: string): Promise<ToDoRepositoryResponse> {
-        try {
-            const toDo = await prisma.toDo.findUnique({
-                where: {
-                    id: toDoId,
-                },
-            });
+	async getById (toDoId: string): Promise<ToDoRepositoryResponse> {
+		try {
+			const toDo = await prisma.toDo.findUnique({
+				where: {
+					id: toDoId,
+				},
+			});
 
-            if (toDo) {
-                return {
-                    success: true,
-                    toDoEntity: this.getToDoEntityFromPrismaToDo(toDo),
-                };
-            }
-        } catch (error) {
-            return {
-                success: false,
-                error,
-            };
-        }
-    }
+			if (toDo) {
+				return {
+					success: true,
+					toDoEntity: this.getToDoEntityFromPrismaToDo(toDo),
+				};
+			}
+		} catch (error) {
+			return {
+				success: false,
+				error,
+			};
+		}
+	}
 
-    async create(newToDo: ToDoEntity): Promise<ToDoRepositoryResponse> {
-        try {
-            const toDo = await prisma.toDo.create({
-                data: {
-                    id: randomUUID(),
-                    user_id: newToDo.getUserId,
-                    title: newToDo.getTitle,
-                    description: newToDo.getDescription,
-                    done: newToDo.getDone,
-                    created_at: newToDo.getCreatedAt,
-                },
-            });
+	async create (newToDo: ToDoEntity): Promise<ToDoRepositoryResponse> {
+		try {
+			const toDo = await prisma.toDo.create({
+				data: {
+					id: randomUUID(),
+					user_id: newToDo.getUserId,
+					title: newToDo.getTitle,
+					description: newToDo.getDescription,
+					done: newToDo.getDone,
+					created_at: newToDo.getCreatedAt,
+				},
+			});
 
-            if (toDo) {
-                return {
-                    success: true,
-                    toDoEntity: this.getToDoEntityFromPrismaToDo(toDo),
-                };
-            }
-        } catch (error) {
-            return {
-                success: false,
-                error,
-            };
-        }
-    }
+			if (toDo) {
+				return {
+					success: true,
+					toDoEntity: this.getToDoEntityFromPrismaToDo(toDo),
+				};
+			}
+		} catch (error) {
+			return {
+				success: false,
+				error,
+			};
+		}
+	}
 
-    async save(toDo: ToDoEntity): Promise<ToDoRepositoryResponse> {
-        try {
-            const queryResponse = await prisma.toDo.update({
-                where: {
-                    id: toDo.getId,
-                },
-                data: {
-                    title: toDo.getTitle,
-                    description: toDo.getDescription,
-                    done: toDo.getDone,
-                },
-            });
+	async save (toDo: ToDoEntity): Promise<ToDoRepositoryResponse> {
+		try {
+			const queryResponse = await prisma.toDo.update({
+				where: {
+					id: toDo.getId,
+				},
+				data: {
+					title: toDo.getTitle,
+					description: toDo.getDescription,
+					done: toDo.getDone,
+				},
+			});
 
-            if (queryResponse) return { success: true, toDoEntity: this.getToDoEntityFromPrismaToDo(queryResponse) };
-        } catch (error) {
-            return { success: false, error };
-        }
-    }
+			if (queryResponse) return { success: true, toDoEntity: this.getToDoEntityFromPrismaToDo(queryResponse) };
+		} catch (error) {
+			return { success: false, error };
+		}
+	}
 
-    async deleteById(toDoId: string): Promise<ToDoRepositoryResponse> {
-        try {
-            const queryResponse = await prisma.toDo.delete({
-                where: {
-                    id: toDoId,
-                },
-            });
+	async deleteById (toDoId: string): Promise<ToDoRepositoryResponse> {
+		try {
+			const queryResponse = await prisma.toDo.delete({
+				where: {
+					id: toDoId,
+				},
+			});
 
-            if (queryResponse) return { success: true };
-        } catch (error) {
-            return { success: false, error };
-        }
-    }
+			if (queryResponse) return { success: true };
+		} catch (error) {
+			return { success: false, error };
+		}
+	}
 }
