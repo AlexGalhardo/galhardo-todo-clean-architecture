c139b5fb93bc62c35a85ffd09faee2291ea62f5b
Alex Galhardo <alex.vieira@pravaler.com.br>
2023-02-23 14:29:03 -0300

    fix: updated linter and todocontroller name

--- src/domain/todo/ToDoController.ts
+++ src/domain/todo/ToDoController.ts
@@ -10,64 +10,64 @@ import GetToDoByIdUseCase from "./GetToDoByIdUseCase";
 import UpdateToDoByIdUseCase from "./UpdateToDoByIdUseCase";
 
 export default class ToDoController {
-	static async getAllToDos (req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
-		const { success, toDos, error } = await new GetAllToDosUseCase().execute(getDecodedJwtToken(req).userId);
+    static async getAllToDos(req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
+        const { success, toDos, error } = await new GetAllToDosUseCase().execute(getDecodedJwtToken(req).userId);
 
-		return res
-			.status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
-			.json(success ? { success: true, toDos } : { success: false, error });
-	}
+        return res
+            .status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
+            .json(success ? { success: true, toDos } : { success: false, error });
+    }
 
-	static async getById (req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
-		const { todo_id } = req.params;
+    static async getById(req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
+        const { todo_id } = req.params;
 
-		const { success, toDo, error } = await new GetToDoByIdUseCase().execute(
-			getDecodedJwtToken(req).userId,
-			todo_id,
-		);
+        const { success, toDo, error } = await new GetToDoByIdUseCase().execute(
+            getDecodedJwtToken(req).userId,
+            todo_id,
+        );
 
-		return res
-			.status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
-			.json(success ? { success: true, toDo } : { success: false, error });
-	}
+        return res
+            .status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
+            .json(success ? { success: true, toDo } : { success: false, error });
+    }
 
-	static async create (req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
-		const { title, description, done } = req.body;
+    static async create(req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
+        const { title, description, done } = req.body;
 
-		const { success, toDo, error } = await new CreateToDoUseCase().execute({
-			userId: getDecodedJwtToken(req).userId,
-			title,
-			description,
-			done,
-		});
+        const { success, toDo, error } = await new CreateToDoUseCase().execute({
+            userId: getDecodedJwtToken(req).userId,
+            title,
+            description,
+            done,
+        });
 
-		return res
-			.status(success ? HttpStatusCode.CREATED : HttpStatusCode.BAD_REQUEST)
-			.json(success ? { success: true, toDo } : { success: false, error });
-	}
+        return res
+            .status(success ? HttpStatusCode.CREATED : HttpStatusCode.BAD_REQUEST)
+            .json(success ? { success: true, toDo } : { success: false, error });
+    }
 
-	static async updateById (req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
-		const { id, title, description, done } = req.body;
+    static async updateById(req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
+        const { id, title, description, done } = req.body;
 
-		const { success, toDo, error } = await new UpdateToDoByIdUseCase().execute({
-			id,
-			title,
-			description,
-			done,
-		});
+        const { success, toDo, error } = await new UpdateToDoByIdUseCase().execute({
+            id,
+            title,
+            description,
+            done,
+        });
 
-		return res
-			.status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
-			.json(success ? { success: true, toDo } : { success: false, error });
-	}
+        return res
+            .status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
+            .json(success ? { success: true, toDo } : { success: false, error });
+    }
 
-	static async deleteById (req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
-		const { todo_id } = req.params;
+    static async deleteById(req: Request, res: Response): Promise<Response<IToDoUseCaseDefaultResponse>> {
+        const { todo_id } = req.params;
 
-		const { success, error } = await new DeleteToDoByIdUseCase().execute(todo_id);
+        const { success, error } = await new DeleteToDoByIdUseCase().execute(todo_id);
 
-		return res
-			.status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
-			.json(success ? { success: true, status: `ToDo Id: ${todo_id} deleted` } : { success: false, error });
-	}
+        return res
+            .status(success ? HttpStatusCode.OK : HttpStatusCode.BAD_REQUEST)
+            .json(success ? { success: true, status: `ToDo Id: ${todo_id} deleted` } : { success: false, error });
+    }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,14 @@ import UserController from "./domain/user/UserController";
 import { userIsAuthenticated } from "./middlewares/userIsAuthenticated";
 
 export default Router()
-	.post("/user/register", UserController.register)
-	.post("/user/login", UserController.login)
-	.post("/user/logout", userIsAuthenticated, UserController.logout)
-	.put("/user/update", userIsAuthenticated, UserController.update)
-	.delete("/user/delete/:user_id", userIsAuthenticated, UserController.deleteById)
+    .post("/user/register", UserController.register)
+    .post("/user/login", UserController.login)
+    .post("/user/logout", userIsAuthenticated, UserController.logout)
+    .put("/user/update", userIsAuthenticated, UserController.update)
+    .delete("/user/delete/:user_id", userIsAuthenticated, UserController.deleteById)
 
-	.get("/todo/all", userIsAuthenticated, ToDoController.getAllToDos)
-	.get("/todo/:todo_id", userIsAuthenticated, ToDoController.getById)
-	.post("/todo/create", userIsAuthenticated, ToDoController.create)
-	.put("/todo/update", userIsAuthenticated, ToDoController.updateById)
-	.delete("/todo/delete/:todo_id", userIsAuthenticated, ToDoController.deleteById);
+    .get("/todo/all", userIsAuthenticated, ToDoController.getAllToDos)
+    .get("/todo/:todo_id", userIsAuthenticated, ToDoController.getById)
+    .post("/todo/create", userIsAuthenticated, ToDoController.create)
+    .put("/todo/update", userIsAuthenticated, ToDoController.updateById)
+    .delete("/todo/delete/:todo_id", userIsAuthenticated, ToDoController.deleteById);
