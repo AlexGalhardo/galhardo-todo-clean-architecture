aca252f634d6a8de6f285fe9b72087a3e8870c7f
Alex Galhardo <alex.vieira@pravaler.com.br>
2023-02-23 14:28:02 -0300

    fix: todoscontroller name

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
 
-import ToDosController from "./domain/todo/ToDosController";
+import ToDoController from "./domain/todo/ToDoController";
 import UserController from "./domain/user/UserController";
 import { userIsAuthenticated } from "./middlewares/userIsAuthenticated";
 
 export default Router()
-    .post("/user/register", UserController.register)
-    .post("/user/login", UserController.login)
-    .post("/user/logout", userIsAuthenticated, UserController.logout)
-    .put("/user/update", userIsAuthenticated, UserController.update)
-    .delete("/user/delete/:user_id", userIsAuthenticated, UserController.deleteById)
+	.post("/user/register", UserController.register)
+	.post("/user/login", UserController.login)
+	.post("/user/logout", userIsAuthenticated, UserController.logout)
+	.put("/user/update", userIsAuthenticated, UserController.update)
+	.delete("/user/delete/:user_id", userIsAuthenticated, UserController.deleteById)
 
-    .get("/todo/all", userIsAuthenticated, ToDosController.getAllToDos)
-    .get("/todo/:todo_id", userIsAuthenticated, ToDosController.getById)
-    .post("/todo/create", userIsAuthenticated, ToDosController.create)
-    .put("/todo/update", userIsAuthenticated, ToDosController.updateById)
-    .delete("/todo/delete/:todo_id", userIsAuthenticated, ToDosController.deleteById);
+	.get("/todo/all", userIsAuthenticated, ToDoController.getAllToDos)
+	.get("/todo/:todo_id", userIsAuthenticated, ToDoController.getById)
+	.post("/todo/create", userIsAuthenticated, ToDoController.create)
+	.put("/todo/update", userIsAuthenticated, ToDoController.updateById)
+	.delete("/todo/delete/:todo_id", userIsAuthenticated, ToDoController.deleteById);
